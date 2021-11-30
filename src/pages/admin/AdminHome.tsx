f94e9ca82c8fe42ce9e19855090ac8c92cf7db56
Artūras <arturassisakovas@gmail.com>
2021-11-30 05:56:24 +0200

    added logic for admin panel modal open when reseting clients password

--- src/pages/admin/AdminHome.tsx
+++ src/pages/admin/AdminHome.tsx
@@ -1,11 +1,36 @@
 import React, {useEffect, useState} from 'react';
 import {useTranslation} from "react-i18next";
-import {Table} from "react-bootstrap";
+import {Button, Modal, Table} from "react-bootstrap";
 
 const AdminHome = () => {
     const {t} = useTranslation("home");
+    const [show, setShow] = useState(false);
     const [users, setUsers] = useState([]);
+    const [userEmail, setUserEmail] = useState('');
+    const [userName, setUserName] = useState('');
+    const [userNewPassword, setUserNewPassword] = useState('');
     const [error, setError] = useState(false);
+    const handleClose = () => setShow(false);
+    const reload=() => window.location.reload();
+    const handleShow = async (id: any) => {
+        const response = await fetch('http://localhost:8000/api/user/' + id + '/resetPassword', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            credentials: 'include'
+        })
+        const userData = await response.json();
+        if (response.status >= 400) {
+            setError(true);
+            setShow(true);
+        }
+        else {
+            setUserEmail(userData.email);
+            setUserName(userData.name);
+            setUserNewPassword(userData.generatedPassword);
+            setError(false);
+            setShow(true);
+        }
+    }
 
     useEffect( () => {
         (
@@ -41,24 +66,47 @@ const AdminHome = () => {
                         <thead>
                         <tr>
                             <th>#</th>
-                            <th>{t("user.name")}</th>
-                            <th>{t("user.email")}</th>
-                            <th>{t("user.date.updated")}</th>
+                            <th className="text-center">{t("user.name")}</th>
+                            <th className="text-center">{t("user.email")}</th>
+                            <th className="text-center">{t("user.2fa.status")}</th>
+                            <th className="text-center">{t("user.date.updated")}</th>
+                            <th className="text-center">{t("user.credentials.reset.header")}</th>
                         </tr>
                         </thead>
                         <tbody>
                         {
-                            users.map(({updatedAt, email, name, id}, index) =>
+                            users.map(({updatedAt, email, twoFaEnabled, name, id}, index) =>
                                 <tr key={id}>
-                                    <td>{++index}</td>
-                                    <td>{name}</td>
-                                    <td>{email}</td>
-                                    <td>{updatedAt}</td>
+                                    <td className="text-center">{++index}</td>
+                                    <td className="text-center">{name}</td>
+                                    <td className="text-center">{email}</td>
+                                    <td className="text-center">{t(twoFaEnabled ? "twoFa.yes" : "twoFa.no")}</td>
+                                    <td className="text-center">{updatedAt}</td>
+                                    <td className="text-center">{<Button className=" btn btn-lg btn-primary mb-1" type="submit" onClick={() => handleShow(id)}>{t("user.credentials.reset")}</Button>}</td>
                                 </tr>
                             )
                         }
                         </tbody>
                     </Table>
+                    <>
+                        <Modal centered={true} size="lg" show={show} onHide={handleClose} onExit={reload}>
+                            <Modal.Header>
+                                <Modal.Title>{t("password.changed.modal.heading")}</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <>
+                                    <p>{t("password.changed.for.user")} <strong>{userName}</strong></p>
+                                    <p>{t("password.changed.for.user.email")} <strong>{userEmail}</strong></p>
+                                    <p>{t("password.changed.for.user.password")} <strong>{userNewPassword}</strong></p>
+                                </>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button variant="secondary" onClick={handleClose}>
+                                    {t("password.change.modal.close")}
+                                </Button>
+                            </Modal.Footer>
+                        </Modal>
+                    </>
                 </>
             )
     }
